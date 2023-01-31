301889efb3a37827cdd7e1629585c7a55941b0a2
Michelle Ross <miross629@gmail.com>
2023-01-31 14:32:01 -0800

    Redirect user from /tasks to /sign-in

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useUser } from '../../context/UserContext.js';
 
 export default function Tasks() {
+  const { user } = useUser;
+
+  // send not authorized users to sign in page
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
   return (
     <div>
       <p>Tasks Go Here</p>
