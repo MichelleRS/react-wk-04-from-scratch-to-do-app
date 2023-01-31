b46d7356711335acbdf24a2708bc121f0f191f6c
Michelle Ross <miross629@gmail.com>
2023-01-31 14:04:54 -0800

    Begin nav based on user Auth, TODO: figure out why user is not being signed out on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext.js';
+import { signOut } from '../services/auth.js';
 
 export default function Header() {
+  const { user, setUser } = useContext(UserContext);
+
+  //   use as on click sign out function
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      setUser(null);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
   return (
     <header>
-      <h1>Hello! I am the Header</h1>
+      <h1>To Do List</h1>
+
+      {/* temp */}
+      <button type="button" onClick={handleSignOut}>
+        Sign Out
+      </button>
+
+      <nav>
+        {/* nav for when NOT an auth user */}
+
+        {/* nav for when auth user */}
+        {user && (
+          <>
+            <p>Hey there {user.email}!</p>
+            {/* button handles click for sign out */}
+            <button type="button" onClick={handleSignOut}>
+              Sign Out
+            </button>
+          </>
+        )}
+      </nav>
     </header>
   );
 }
