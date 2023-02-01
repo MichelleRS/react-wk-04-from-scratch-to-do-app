809b2cfaf3aec70b932f489d5eed1e53429c9313
Michelle Ross <miross629@gmail.com>
2023-02-01 15:44:23 -0800

    Display task in list, TODO: connect task with user_id in Supabase

--- src/components/Tasks/TasksForm.js
+++ src/components/Tasks/TasksForm.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { TasksContext } from '../../context/TasksContext.js';
+import { createTask } from '../../services/tasks.js';
 
 export default function TasksForm() {
   // TODO add state
+  const [task, setTask] = useState('');
+  const { setTasks } = useContext(TasksContext);
 
   // TODO function for handling button click
+  const handleTaskInput = async () => {
+    try {
+      const input = await createTask(task);
+      setTasks((prev) => [...prev, input]);
+      setTask('');
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
 
   return (
     <>
       <form>
         {/* task input */}
         <label>Task:</label>
-        <input type="text" minLength={2}></input>
+        <input
+          type="text"
+          minLength={2}
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        ></input>
         {/* submit input button */}
-        <button type="submit">Submit Task</button>
+        <button type="submit" onClick={handleTaskInput}>
+          Submit Task
+        </button>
       </form>
     </>
   );
