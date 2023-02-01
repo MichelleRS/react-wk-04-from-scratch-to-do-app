536ae34bd2997cf2336181317d76a42c8a4d02ba
Michelle Ross <miross629@gmail.com>
2023-01-31 17:49:43 -0800

    Create insert task function

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -4,3 +4,8 @@ export async function getTasksList() {
   const response = await client.from('todo_list').select('*');
   return checkError(response);
 }
+
+export async function createTask(task) {
+  const response = await client.from('todo_list').insert({ task }).single();
+  return checkError(response);
+}
